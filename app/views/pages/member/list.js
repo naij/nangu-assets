616c150902e97f3eaa73a262283b9d2812ccbdd2
naij <naijser@gmail.com>
2020-01-09 17:17:04 +0800

    新增商户绑定

--- app/views/pages/member/list.js
+++ app/views/pages/member/list.js
@@ -1,8 +1,10 @@
 var Magix = require('magix')
 var $ = require('jquery')
+var Dialog = require('app/mixins/dialog')
 
 module.exports = Magix.View.extend({
   tmpl: '@list.html',
+  mixins: [Dialog],
   ctor: function() {
     this.observe(['pageNo'])
   },
@@ -31,5 +33,51 @@ module.exports = Magix.View.extend({
   },
   'pageChange<change>': function(e) {
     this.to({pageNo: e.state.page})
+  },
+  'bind<click>': function(e) {
+    e.preventDefault()
+    var me = this
+    var unionId = e.params.unionId
+    me.mxDialog('app/views/pages/common/business_picker', {
+      width: 700,
+      callback: function(data) {
+        me.alert('商户绑定成功！')
+        me.request().all([{
+          name: 'member_update',
+          params: {
+            unionId: unionId,
+            businessId: data.id
+          }
+        }], function(e, MesModel) {
+          me.render()
+        })
+      }
+    })
+  },
+  'unbind<click>': function(e) {
+    e.preventDefault()
+    var me = this
+    var unionId = e.params.unionId
+    me.confirm('确定要解绑此商户？', function () {
+      me.request().all([{
+        name: 'member_update',
+        params: {
+          unionId: unionId,
+          businessId: ''
+        }
+      }], function(e, MesModel) {
+        me.alert('商户解绑成功！')
+        me.render()
+      })
+    })
+  },
+  filters: {
+    formatType: function(value) {
+      if (value) {
+        return '<span class="color-green">商户</span>'
+      } else {
+        return '<span class="color-m">普通用户</span>'
+      }
+    }
   }
 })
